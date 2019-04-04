c85a9d3bffc17064c1065d3af9c39e8423e48063
David Reese <dave@freshconsulting.com>
2019-04-04 15:34:38 -0700

    clean slate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import styles from './App.module.scss';
+
+// import styles from './App.module.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <header className={styles['App-header']}>
-          <img src={logo} className={styles['App-logo']} alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className={styles['App-link']}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div />
     );
   }
 }
