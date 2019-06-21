c10ca58952190c8de00c32d7bdf15f1716057458
David Reese <dave@freshconsulting.com>
2019-06-20 23:15:38 -0700

    commas

--- src/views/404/NoMatch.tsx
+++ src/views/404/NoMatch.tsx
@@ -8,7 +8,7 @@ class NoMatch extends React.Component<any, any> {
     return (
       <section className={styles.noMatch}>
         <h1>At least we don't have to dump any beer because of this.</h1>
-        <p>Not sure what happened here, but you can always head back to the dashboard or, of course, brew beer.</p>
+        <p>Not sure what happened here, but you can always head back to the dashboard, or of course, brew beer.</p>
         <p>
           <Link
             to="dashboard"
