b04b052ce3765e4e0ec775857bace3f5e8d85af2
David Reese <dave@freshconsulting.com>
2019-06-20 22:44:37 -0700

    calculator search

--- src/views/Calculators/Calculators.tsx
+++ src/views/Calculators/Calculators.tsx
@@ -21,11 +21,32 @@ import IBU from './Calculators/ibu';
 import SRM from './Calculators/srm';
 import CO2 from './Calculators/co2';
 
+const calculatorArray = [
+  {calculator: <TotalWater calculator={Calculator.totalWater} />, terms: ['water', 'total']},
+  {calculator: <StrikeVolume calculator={Calculator.strikeVolume} />, terms: ['water', 'strike']},
+  {calculator: <StrikeTemperature calculator={Calculator.strikeTemp} />, terms: ['water', 'strike']},
+  {calculator: <SpargeVolume calculator={Calculator.spargeVolume} />, terms: ['water', 'sparge']},
+  {calculator: <EvaporationPercent calculator={Calculator.evaporationPercent} />, terms: ['water', 'boil']},
+  {calculator: <PreBoilVolume calculator={Calculator.preBoilVol} />, terms: ['water', 'boil']},
+  {calculator: <OriginalGravity calculator={Calculator.OG} />, terms: ['gravity', 'original', 'og']},
+  {calculator: <PreBoilGravity calculator={Calculator.preBoilG} />, terms: ['gravity', 'pre boil', 'pre-boil']},
+  {calculator: <YeastTargetPitchingRate calculator={Calculator.targetPitchingRate} />, terms: ['yeast', 'pitching rate']},
+  {calculator: <YeastPitchingRate calculator={Calculator.pitchingRate} />, terms: ['yeast', 'pitching rate']},
+  {calculator: <FinalGravity calculator={Calculator.FG} />, terms: ['gravity', 'final', 'fg']},
+  {calculator: <AlcoholContent calculator={Calculator.alcoholContent} />, terms: ['alcohol', 'abv']},
+  {calculator: <ApparentAttenuation calculator={Calculator.attenuation} />, terms: ['attenuation', 'yeast']},
+  {calculator: <IBU calculator={Calculator.IBU} />, terms: ['ibu', 'ibus', 'hops']},
+  {calculator: <SRM calculator={Calculator.SRM} />, terms: ['srm', 'color', 'malt']},
+  {calculator: <CO2 calculator={Calculator.CO2} />, terms: ['co2', 'packaging', 'carbonation']},
+];
+
 class Calculators extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
 
-    this.state = {}
+    this.state = {
+      search: '',
+    }
   }
 
   componentDidMount() {
@@ -33,60 +54,44 @@ class Calculators extends React.Component<any, any> {
   }
 
   render() {
+    const filtered = calculatorArray.filter((item, i) => {
+      let result;
+      if (this.state.search !== '' && item.terms.find(term => term.includes(this.state.search))) {
+        result = item;
+      } else if (this.state.search === '') {
+        result = item;
+      }
+      return result;
+    });
+
     return (
       <section className={styles.calculators}>
         <div className={styles.topRow}>
           <h1 className={styles.calculators__header}>Calculators</h1>
+          <input
+            type="text"
+            name="search"
+            className={styles.search}
+            placeholder="Search"
+            onChange={(e: any) => {this.setState({search: e.target.value})}}
+          />
         </div>
         <div className={styles.calculators__container}>
-          <Card customClass={styles.card}>
-            <TotalWater calculator={Calculator.totalWater} />
-          </Card>
-          <Card customClass={styles.card}>
-            <StrikeVolume calculator={Calculator.strikeVolume} />
-          </Card>
-          <Card customClass={styles.card}>
-            <StrikeTemperature calculator={Calculator.strikeTemp} />
-          </Card>
-          <Card customClass={styles.card}>
-            <SpargeVolume calculator={Calculator.spargeVolume} />
-          </Card>
-          <Card customClass={styles.card}>
-            <EvaporationPercent calculator={Calculator.evaporationPercent} />
-          </Card>
-          <Card customClass={styles.card}>
-            <PreBoilVolume calculator={Calculator.preBoilVol} />
-          </Card>
-          <Card customClass={styles.card}>
-            <OriginalGravity calculator={Calculator.OG} />
-          </Card>
-          <Card customClass={styles.card}>
-            <PreBoilGravity calculator={Calculator.preBoilG} />
-          </Card>
-          <Card customClass={styles.card}>
-            <YeastTargetPitchingRate calculator={Calculator.targetPitchingRate} />
-          </Card>
-          <Card customClass={styles.card}>
-            <YeastPitchingRate calculator={Calculator.pitchingRate} />
-          </Card>
-          <Card customClass={styles.card}>
-            <FinalGravity calculator={Calculator.FG} />
-          </Card>
-          <Card customClass={styles.card}>
-            <AlcoholContent calculator={Calculator.alcoholContent} />
-          </Card>
-          <Card customClass={styles.card}>
-            <ApparentAttenuation calculator={Calculator.attenuation} />
-          </Card>
-          <Card customClass={styles.card}>
-            <IBU calculator={Calculator.IBU} />
-          </Card>
-          <Card customClass={styles.card}>
-            <SRM calculator={Calculator.SRM} />
-          </Card>
-          <Card customClass={styles.card}>
-            <CO2 calculator={Calculator.CO2} />
-          </Card>
+          {filtered.length > 0 
+            ? filtered.map((item, i) => {
+                if (this.state.search !== '' && item.terms.find(term => term.includes(this.state.search))) {
+                  return <Card customClass={styles.card} key={i}>
+                      {item.calculator}
+                    </Card>;
+                } else if (this.state.search === '') {
+                  return <Card customClass={styles.card} key={i}>
+                      {item.calculator}
+                    </Card>;
+                } else {
+                  return null;
+                }
+              })
+            : <p className={styles.noResults}>No results round.</p>}
         </div>
       </section>
     );
